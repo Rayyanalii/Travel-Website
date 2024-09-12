20f695321191aabe7b19a951784314016b8d676d
Rayyanalii <rayyanali2705@gmail.com>
2024-09-12 22:45:07 +0500

    Navbar Created, AppJSX started

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+    <nav className="navbar-container">
+      <div className="container">
+        <div className="logo">
+          <a href="/"><img src="src\assets\MajesticTravels Logo.png" alt="Logo" /></a>
+        </div>
+        <div className="rightSide">
+        <div className="links">
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/">Destinations</a></li>
+            <li><a href="/">Services</a></li>
+            <li><a href="/">Pages</a></li>
+          </ul>
+          </div>
+          <div className="verticalLine">
+            |
+        </div>
+        <div className="buttons">
+          <button className="mainButtons" id="loginButton">Login</button>
+          <button className="mainButtons" id="signupButton">Signup</button>
+        </div>
+        </div>
+        
+      </div>
+    </nav>
+    </>
   )
 }
 
