403e192a50e649650e7b420afa1473509b020e21
Rayyanalii <rayyanali2705@gmail.com>
2024-09-21 20:14:29 +0500

    Completed App.JSX responsiveness, modified Navbar to add Mobile Hamburger Menu

--- src/App.jsx
+++ src/App.jsx
@@ -56,7 +56,7 @@ function App() {
         </div>
         <div className="allServices">
           <ServiceCard title="Rent A Car" desc="Choose from our finest car rentals choices" iden="car"/>
-          <ServiceCard title="Book A Flight" desc="Flight booking made easy" iden="plane"/>
+          <ServiceCard title="Book A Flight" desc="Travel in the finest airlines business class" iden="plane"/>
           <ServiceCard title="Book A Hotel" desc="Luxurious and affordable hotels available for you" iden="hotel"/>
         </div>
       </main>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,71 @@
 import React from 'react'
 import './Navbar.css'
 import Destinations from '../pages/Destinations'
+import { useState } from 'react'
+import { GiHamburgerMenu } from "react-icons/gi";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [hamburger, sethamburger] = useState(false);
+  const [serviceDrop, setserviceDrop] = useState(false);
+
+  function openHamburger() {
+    sethamburger(!hamburger);
+  }
+
+  function openservice() {
+    setserviceDrop(!serviceDrop);
+  }
+
+
   return (
     <>
-    <nav className="navbar-container">
-      <div className="container">
-        <div className="logo">
-          <a href="/"><img src="src\assets\MajesticTravels Logo.png" alt="Logo" /></a>
-        </div>
-        <div className="rightSide">
-        <div className="links">
-          <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/destinations">Destinations</a></li>
-            <li><a href="/">Services</a></li>
-            <li><a href="/">Pages</a></li>
-          </ul>
+      <nav className="navbar-container">
+        <div className="container">
+          <div className="logo">
+            <Link to="/"><img src="src\assets\MajesticTravels Logo.png" alt="Logo" /></Link>
           </div>
-          <div className="verticalLine">
-            |
+          <div className="rightSide">
+            <div className="links">
+              <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/destinations">Destinations</Link></li>
+                <li className='service'>
+                  <Link to="/" onClick={openservice}>Services</Link>
+                  <div className={serviceDrop ? "serviceDropdown" : "closedServiceDropdown"}>
+                    Hello
+                  </div>
+                </li>
+                <li><Link to="/">Pages</Link></li>
+              </ul>
+            </div>
+            <div className="verticalLine">
+              |
+            </div>
+            <div className="buttons">
+              <button className="mainButtons" id="loginButton">Login</button>
+              <button className="mainButtons" id="signupButton">Signup</button>
+            </div>
+          </div>
         </div>
-        <div className="buttons">
-          <button className="mainButtons" id="loginButton">Login</button>
-          <button className="mainButtons" id="signupButton">Signup</button>
-        </div>
-        </div>
-        
-      </div>
 
-    </nav>
+        <div className="mobileContainer">
+          <div className="hamburgerIconContainer">
+            <GiHamburgerMenu className="hamburger" onClick={openHamburger} />
+          </div>
+          <div className="mobileLogo">
+            <a href="/"><img src="src\assets\MajesticTravels Logo.png" alt="Logo" /></a>
+          </div>
+        </div>
+        <div className={hamburger ? "sliderMenuContainer" : "closedSlider"}>
+          <ul>
+            <li className='menuListItems'>Home</li>
+            <li className='menuListItems'>Destination</li>
+            <li className='menuListItems'>Services</li>
+            <li className='menuListItems'>Pages</li>
+          </ul>
+        </div>
+      </nav>
     </>
   )
 }
